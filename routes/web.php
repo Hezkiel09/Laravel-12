ea6cc53ed8de84328eb6b8e395f6471d34219c7d
Hezkiel09 <kielsiahaan09@gmail.com>
2025-12-31 12:32:26 +0700

    Routing + About page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// About
+Route::get('/about',function () {
+    return view('about');
+});
+
+Route::get('/ping', function(){
+    return response()->json(['message' => 'Halo Mok',
+                                    'Status' => 'Lapar Mok'
+]);
+});
